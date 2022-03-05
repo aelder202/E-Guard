b8ea8f98c9c18fa9bc2770c0a7a446e175d66159
Taylor Elder <54559165+aelder202@users.noreply.github.com>
2022-03-04 21:16:56 -0800

    Update GUI
    - Exception added for processes needing admin privileges to close -- UNTESTED
    
    - Console no longer prints output from the same source IP more than once

--- Windows/main.py
+++ Windows/main.py
@@ -12,11 +12,13 @@ class KeylogDetector:
         self.whitelist_ip = ['127.0.0.1', '0.0.0.0']
         self.blacklist = []
         self.blacklist_ip = []
-        self.source_ip = []
+        self.source_ip = ['127.0.0.1', '0.0.0.0']
+        self.process_name = None
         self.grouped_output = None
         self.skip_print = False
         self.output = None
         self.stop_gui = None
+        self.p = psutil.Process()
         self.gui = master
         self.timer = 1
         gui.geometry('700x460')
@@ -59,30 +61,33 @@ class KeylogDetector:
         # stop show_output for 1 second before calling itself again  << double check this
         self.stop_gui = gui.after(100, self.show_output)
         if "ESTABLISHED" in self.output:
-            # group output by whitespace to extract data (IP, port, etc.)
             self.grouped_output = self.output.split(" ")
-            # check if the IP has been logged in whitelist_ip
-            self.check_list()
-            if not self.skip_print:
-                self.out_box.insert(INSERT, self.output)
+            # delete empty array elements
+            self.grouped_output = list(filter(None, self.grouped_output))
+            # let application terminate if it exists in blacklist
+            if self.process_name in self.blacklist:
                 self.stop_output()
                 self.run_keylog()
+            # make sure the source_ip hasn't been printed to console more than once
+            if not any(x in self.source_ip for x in self.grouped_output):
+                # check if the IP has been logged in whitelist_ip
+                self.check_list()
+                if not self.skip_print:
+                    self.out_box.insert(INSERT, self.output)
+                    self.stop_output()
+                    self.run_keylog()
         # autoscroll feature
         self.out_box.see(END)
 
     def check_list(self):
         # if IP is in whitelist, go back to the beginning of show_output
-        my_list = self.grouped_output
-        # delete empty array elements
-        my_list = list(filter(None, my_list))
-        # obtain IP with port number
-        ip_addr = my_list[-3]
+        ip_addr = self.grouped_output[-3]
         # separate IP from port
         get_ip = ip_addr.split(":")
         # assign IP to check if this exists in whitelist_ip
         ip = [get_ip[0]]
         # if yes, do not print output or go to run_keylog and continue scanning
-        if set(ip).issubset(set(self.whitelist_ip)):
+        if any(x in ip for x in self.whitelist_ip):
             self.skip_print = True
 
     def stop_output(self):
@@ -92,65 +97,71 @@ class KeylogDetector:
         self.out_box.see(END)
 
     def run_keylog(self):
-        my_list = self.grouped_output
-        # delete empty array elements
-        my_list = list(filter(None, my_list))
-        # PID will be the last number once split
-        pid = my_list[-1]
+        pid = self.grouped_output[-1]
         # get application name using PID
         cmd_output = subprocess.getoutput(f'tasklist /fi "pid eq {pid}"')
         # split to obtain process name
-        process_name = cmd_output.split()
+        self.process_name = cmd_output.split()
         # identify source_ip to prevent the same application from showing twice in output
-        if my_list[-3] not in self.source_ip:
-            self.source_ip.append(my_list[-3])
+        if self.grouped_output[-3] not in self.source_ip:
+            self.source_ip.append(self.grouped_output[-3])
         # get the full IP address with port number from the last element from output
-        ip_addr = my_list[-3]
+        ip_addr = self.grouped_output[-3]
         # split at the ':' to get port number at last index of array
         get_port = ip_addr.split(":")
         port = get_port[-1]
         # process name is always 13th element in array.
-        process_name = process_name[13]
-        p = psutil.Process(int(pid))
+        self.process_name = self.process_name[13]
+        self.p = psutil.Process(int(pid))
 
-        if process_name not in self.whitelist:
-            self.out_box.insert(INSERT, "\nKeylogger Detected.\n\n")
-            # terminate process if it exists in blacklist
-            if process_name in self.blacklist:
-                p.kill()
-                self.out_box.insert(INSERT, "Blacklist application found running.\nProcess automatically "
-                                            "terminated.\n\n")
-                self.timer = 1
-                self.show_output()
+        if self.process_name not in self.whitelist:
+            try:
+                self.out_box.insert(INSERT, "\nKeylogger Detected.\n\n")
+                # terminate process if it exists in blacklist
+                if self.process_name in self.blacklist:
+                    self.p.kill()
+                    self.out_box.insert(INSERT, "Blacklist application found running.\nProcess automatically "
+                                                "terminated.\n\n")
+                    self.timer = 1
+                    self.show_output()
 
-            # if process is not in whitelist, check if it should be
-            else:
-                self.out_box.insert(INSERT, "Pausing application...\n\n")
-                p.suspend()
-                self.out_box.insert(INSERT, f'Application name: {process_name}\n'
-                                            f'Process ID (PID): {pid}'
-                                            f'Trying to communicate on port {port}\n')
-                self.out_box.see(END)
-
-                is_safe = askyesno(title='Confirmation',
-                                   message="Process marked as dangerous.\nWould you like to add this application to "
-                                           "your whitelist?")
-                if is_safe:
-                    self.out_box.insert(INSERT, "Resuming process...\n")
-                    p.resume()
-                    self.out_box.insert(INSERT, "Adding to whitelist...\n\n")
-                    self.whitelist.append(process_name)
-                    self.whitelist_ip.append(ip_addr)
+                # if process is not in whitelist, check if it should be
                 else:
-                    self.out_box.insert(INSERT, "Terminating process...\n")
-                    p.kill()
-                    self.out_box.insert(INSERT, "Adding to blacklist...\n\n")
-                    self.blacklist.append(process_name)
-                    self.blacklist_ip.append(ip_addr)
+                    self.out_box.insert(INSERT, "Pausing application...\n\n")
+                    self.p.suspend()
+                    self.out_box.insert(INSERT, f'Application name: {self.process_name}\n'
+                                                f'Process ID (PID): {pid}'
+                                                f'Trying to communicate on port {port}\n')
+                    self.out_box.see(END)
 
-                self.out_box.insert(INSERT, f'whitelist: {self.whitelist}\n')
-                self.out_box.insert(INSERT, f'blacklist: {self.blacklist}\n\n')
-                self.out_box.see(END)
+                    is_safe = askyesno(title='Confirmation',
+                                       message="Process marked as dangerous.\nWould you like to add this application "
+                                               "to your whitelist?")
+                    if is_safe:
+                        self.out_box.insert(INSERT, "Resuming process...\n")
+                        self.p.resume()
+                        self.out_box.insert(INSERT, "Adding to whitelist...\n\n")
+                        self.whitelist.append(self.process_name)
+                        self.whitelist_ip.append(ip_addr)
+                    else:
+                        self.out_box.insert(INSERT, "Terminating process...\n")
+                        self.p.kill()
+                        self.out_box.insert(INSERT, "Adding to blacklist...\n\n")
+                        self.blacklist.append(self.process_name)
+                        self.blacklist_ip.append(ip_addr)
+
+                    self.out_box.insert(INSERT, f'whitelist: {self.whitelist}\n')
+                    self.out_box.insert(INSERT, f'blacklist: {self.blacklist}\n\n')
+                    self.out_box.see(END)
+                    self.timer = 1
+                    self.show_output()
+
+            except psutil.AccessDenied:
+                self.out_box.insert(INSERT, "\nApplication requires administrator privileges to stop. Unable to "
+                                            "continue.\n")
+                self.out_box.insert(INSERT, "Adding to whitelist...\n\n")
+                self.whitelist.append(self.process_name)
+                self.whitelist_ip.append(ip_addr)
                 self.timer = 1
                 self.show_output()
 
